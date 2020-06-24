1326e4ab26db8e56f8bcf9ca641ba025f190b83f
Alaina Shumate <ashumate@salz3.idies.jhu.edu>
2020-06-24 13:30:56 -0400

    edit version number

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 setuptools.setup(
 	name="Liftoff",
-	version="1.0.0",
+	version="1.1.0",
 	author="Alaina Shumate",
 	author_email="ashumat2@jhmi.edu",
-	description="A genome annotation liftover tool",
+	description="A gene annotation mapping tool",
 	url="https://github.com/ashumate/Liftoff",
 	install_requires=['numpy', 'biopython','gffutils', 'networkx','pysam'],
-	python_requires='>=3.7'
+	python_requires='>=3.0'
 )
