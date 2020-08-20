e152bf99e146b377340ea599973ca3a32f24acac
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2020-08-20 12:19:31 -0400

    Update CLI

--- liftoff/run_liftoff.py
+++ liftoff/run_liftoff.py
@@ -7,8 +7,8 @@ def main():
     if args.chroms is not None:
         ref_chroms, target_chroms = parse_chrm_files(args.chroms)
     else:
-        ref_chroms = [args.r]
-        target_chroms = [args.t]
+        ref_chroms = [args.reference]
+        target_chroms = [args.target]
     parent_features_to_lift = get_parent_features_to_lift(args.f)
     lifted_feature_list = {}
     unmapped_features = []
@@ -27,56 +27,92 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Lift features from one genome assembly to another')
-    group = parser.add_mutually_exclusive_group(required=True)
-    parser.add_argument("-V", "--version", help="show program version", action='version', version="v1.3.0")
-    parser.add_argument('-t', required=True, help="target fasta genome to lift genes to", metavar="<target.fasta>")
-    parser.add_argument('-r', required=True, help="reference fasta genome to lift genes from",
-                        metavar="<reference.fasta>")
-    group.add_argument('-g', help="annotation file to lift over in gff or gtf format", metavar="<ref_annotation.gff>")
-    parser.add_argument('-chroms', required=False,
-                        help="comma seperated file with corresponding chromosomes in the reference,target sequences",
-                        metavar="<chroms.txt>")
-    parser.add_argument('-p', required=False, help="processes", default=1, metavar=1, type=int)
-    parser.add_argument('-o', required=False, help="output file", default='stdout', metavar="<output.gff>")
-    group.add_argument('-db',
-                       help="name of feature database. If none, -g argument must be provided and a database will be "
-                            "built automatically")
-    parser.add_argument('-infer_transcripts', action='store_true', required=False,
-                        help="use if GTF file only includes exon/CDS features and does not include transcripts/mRNA")
-    parser.add_argument('-u', required=False, help="name of file to write unmapped features to",
-                        default="unmapped_features.txt", metavar="<unmapped_features.txt>")
-    parser.add_argument('-infer_genes', action='store_true', required=False,
-                        help="use if GTF file only includes transcripts, exon/CDS features")
-    parser.add_argument('-a', required=False, help="minimum alignment coverage to consider a feature mapped [0-1]",
-                        default=0.5, metavar=0.5, type=float)
-    parser.add_argument('-s', required=False,
-                        help="minimum sequence identity in child features (usually exons/CDS) to consider a feature "
-                             "mapped [0-1]",
-                        default=0.5, metavar=0.5, type=float)
-    parser.add_argument('-unplaced', required=False,
-                        help="text file with name(s) of unplaced sequences to map genes from after genes from "
-                             "chromosomes in chroms.txt are mapped",
-                        metavar="<unplaced_seq_names.txt>")
-    parser.add_argument('-copies', required=False, action='store_true',
-                        help="look for extra gene copies in the target genome")
-    parser.add_argument('-sc', required=False,
-                        help='with -copies, minimum sequence identity in exons/CDS for which a gene is considered a '
-                             'copy. Must be greater than -s',
-                        default=1.0, metavar=1.0, type=float)
-    parser.add_argument('-m', required=False, help="Minimap2 path", metavar='PATH')
-    parser.add_argument('-dir', required=False, help="name of directory to save intermediate fasta and SAM files",
-                        default="intermediate_files", metavar="<intermediate_files_dir>")
-    parser.add_argument('-n', required=False, default=50, metavar=50,
-                        help="max number of Minimap2 alignments to consider for each feature", type=int)
-    parser.add_argument('-f', required=False, metavar="feature types", help="list of feature types to lift-over")
-    parser.add_argument('-d', required=False, metavar=2, default=2, help="distance scaling factor. Alignment nodes " \
-                                                                       "father apart "
-                                                              "than this in the target genome will not be connected in "
-                                                              "the graph", type=float)
-    parser.add_argument('-exclude_partial', default=False, action='store_true',
-                        help="write partial mappings below -s and -a threshold to unmapped_features.txt. If true "
-                             "partial/low sequence identity mappings will be included in the gff file with "
-                             "partial_mapaping=True, low_identity=True in comments")
+
+    parser.add_argument('target', help='target fasta genome to lift genes to')
+    parser.add_argument('reference', help='reference fasta genome to lift genes from')
+
+    refrgrp = parser.add_argument_group('Required input (annotation)')
+    mxgrp = refrgrp.add_mutually_exclusive_group(required=True)
+    mxgrp.add_argument(
+        '-g', metavar='GFF', help='annotation file to lift over in GFF or GTF format'
+    )
+    mxgrp.add_argument(
+        '-db', metavar='DB', help='name of feature database; if not specified, the -g '
+        'argument must be provided and a database will be built automatically'
+    )
+
+    outgrp = parser.add_argument_group('Output')
+    outgrp.add_argument(
+        '-o', default='stdout', metavar='FILE',
+        help='write output to FILE in GFF3 format; by default, output is written to terminal (stdout)'
+    )
+    outgrp.add_argument(
+        '-u', default='unmapped_features.txt', metavar='FILE',
+        help='write unmapped features to FILE; default is "unmapped_features.txt"',
+    )
+    outgrp.add_argument(
+        '-exclude_partial', action='store_true',
+        help='write partial mappings below -s and -a threshold to unmapped_features.txt; if true '
+        'partial/low sequence identity mappings will be included in the gff file with '
+        'partial_mapping=True, low_identity=True in comments'
+    )
+    outgrp.add_argument(
+        '-dir', default='intermediate_files', metavar='DIR',
+        help='name of directory to save intermediate fasta and SAM files; default is "intermediate_files"',
+    )
+
+    aligngrp = parser.add_argument_group('Alignment filtering')
+    aligngrp.add_argument(
+        '-a', default=0.5, metavar='A', type=float,
+        help='designate a feature mapped only if it aligns with coverage ≥A; by default A=0.5',
+    )
+    aligngrp.add_argument(
+        '-s', default=0.5, metavar='S', type=float,
+        help='designate a feature mapped only if its child features (usually exons/CDS) align '
+        'with sequence identity ≥S; by default S=0.5'
+    )
+    aligngrp.add_argument(
+        '-n', default=50, metavar='N', type=int,
+        help='consider at most N Minimap2 alignments for each feature; by default N=50'
+    )
+    aligngrp.add_argument(
+        '-d', metavar='D', default=2.0, type=float,
+        help='distance scaling factor; alignment nodes separated by more than a factor of D in '
+        'the target genome will not be connected in the graph; by default D=2.0'
+    )
+
+    parser.add_argument('-V', '--version', help='show program version', action='version', version='v1.3.0')
+    parser.add_argument(
+        '-p', default=1, type=int, metavar='P', help='use P parallel processes to accelerate alignment; by default P=1'
+    )
+    parser.add_argument('-m', help='Minimap2 path', metavar='PATH')
+    parser.add_argument('-f', metavar='TYPES', help='list of feature types to lift over')
+    parser.add_argument(
+        '-infer_genes', action='store_true',
+        help='use if annotation file only includes transcripts, exon/CDS features'
+    )
+    parser.add_argument(
+        '-infer_transcripts', action='store_true', required=False,
+        help='use if annotation file only includes exon/CDS features and does not include transcripts/mRNA'
+    )
+    parser.add_argument(
+        '-chroms', metavar='TXT', help='comma seperated file with corresponding chromosomes in '
+        'the reference,target sequences',
+    )
+    parser.add_argument(
+        '-unplaced', metavar='TXT',
+        help='text file with name(s) of unplaced sequences to map genes from after genes from '
+        'chromosomes in chroms.txt are mapped; default is "unplaced_seq_names.txt"',
+    )
+    parser.add_argument('-copies', action='store_true', help='look for extra gene copies in the target genome')
+    parser.add_argument(
+        '-sc', default=1.0, metavar='SC', type=float,
+        help='with -copies, minimum sequence identity in exons/CDS for which a gene is considered '
+        'a copy; must be greater than -s; default is 1.0',
+    )
+    parser._positionals.title = 'Required input (sequences)'
+    parser._optionals.title = 'Miscellaneous settings'
+    parser._action_groups = [parser._positionals, refrgrp, outgrp, aligngrp, parser._optionals]
     args = parser.parse_args()
     if (float(args.s) > float(args.sc)):
         parser.error("-sc must be greater than or equal to -s")
