24fa4021dff0a81e6ca117b0210c00e69c1fa8ea
Alaina Shumate <ashumate@salz3.idies.jhu.edu>
2020-08-21 14:43:30 -0400

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
 	name="Liftoff",
-	version="1.4.0",
+	version="1.4.1",
 	author="Alaina Shumate",
 	author_email="ashumat2@jhmi.edu",
 	description="A gene annotation mapping tool",
