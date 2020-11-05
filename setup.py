d075ad43ec6a581554bc93c1c65859e89cb0c0c7
Alaina Shumate <ashumate@salz3.idies.jhu.edu>
2020-11-05 13:56:05 -0500

    updated numpy version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
 	author_email="ashumat2@jhmi.edu",
 	description="A gene annotation mapping tool",
 	url="https://github.com/ashumate/Liftoff",
-	install_requires=['numpy==1.19.0', 'biopython==1.76', 'gffutils==0.10.1', 'networkx==2.4', 'pysam==0.16.0.1','pyfaidx==0.5.8','interlap==0.2.6', "ujson==3.2.0"],
+	install_requires=['numpy==1.7.1', 'biopython==1.76', 'gffutils==0.10.1', 'networkx==2.4', 'pysam==0.16.0.1','pyfaidx==0.5.8','interlap==0.2.6', "ujson==3.2.0"],
 	python_requires='>=3.6',
 	packages=['liftoff'],
 	entry_points={'console_scripts': ['liftoff = liftoff.run_liftoff:main'], },
